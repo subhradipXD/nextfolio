cfec4e67a29c667194117b9c667d5977f1afec26
Subhradip <subhradipdas6969@gmail.com>
2024-10-23 11:11:19 +0530

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,12 @@ import { IoCodeSlashOutline } from "react-icons/io5";
 import { IoSchoolOutline } from "react-icons/io5";
 import { PiGitDiffLight } from "react-icons/pi";
 import { TbMessage2 } from "react-icons/tb";
-
+import { Caesar_Dressing } from 'next/font/google';
+const caesarDressing = Caesar_Dressing({
+    weight: '400',
+    subsets: ['latin'],
+    display: 'swap',
+});
 export function Navbar() {
     const links = [
         {
@@ -15,34 +20,35 @@ export function Navbar() {
             href: "#home",
         },
         {
-            title: "About Me",
+            title: "About",
             icon: <IoPersonOutline className="h-full w-full text-neutral-900 dark:text-neutral-300" />,
             href: "#about",
         },
         {
-            title: "My Works",
+            title: "Projects",
             icon: <PiGitDiffLight className="h-full w-full text-neutral-900 dark:text-neutral-300" />,
             href: "#works",
         },
         {
-            title: "My Education",
+            title: "Education",
             icon: <IoSchoolOutline className="h-full w-full text-neutral-900 dark:text-neutral-300" />,
             href: "#education",
         },
         {
-            title: "My Skills",
+            title: "Skills",
             icon: <IoCodeSlashOutline className="h-full w-full text-neutral-900 dark:text-neutral-300" />,
             href: "#skills",
         },
         {
-            title: "Contact Me",
+            title: "Contact",
             icon: <TbMessage2 className="h-full w-full text-neutral-900 dark:text-neutral-300" />,
             href: "#contact",
         },
     ];
 
+
     return (
-        <div className="fixed md:bottom-5 md:w-full flex items-center justify-center bg-transparent bottom-5 right-5 w-auto md:bg-transparent z-50">
+        <div className={`fixed md:bottom-5 md:w-full flex items-center justify-center bg-transparent bottom-5 right-5 w-auto md:bg-transparent z-50 ${caesarDressing.className}`}>
             <FloatingDock items={links} />
         </div>
     );
